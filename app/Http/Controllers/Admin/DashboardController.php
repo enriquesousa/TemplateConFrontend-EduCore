8db0a0cd70e83be16beef78b61d8ff9d98605cf4
Enrique <enrique.sousa@gmail.com>
2025-03-09 09:49:33 -0700

    Commit Inicial

---
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function index(): View
+    {
+        return view('admin.layouts.master');
+        // return view('admin.dashboard');
+    }
+}
