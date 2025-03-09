e121b3b32d4a90de9a8f630d88a5c9af5bd91a02
Enrique <enrique.sousa@gmail.com>
2025-03-09 10:26:10 -0700

    02. Mastering Home Page

---
+++ resources/views/frontend/pages/home/index.blade.php
@@ -0,0 +1,38 @@
+@extends('frontend.layouts.master')
+@section('content')
+
+<!--=== BANNER HERO SECTION ===-->
+@include('frontend.pages.home.sections.hero-section')
+
+<!--=== CATEGORY SECTION ===-->
+{{-- @include('frontend.pages.home.sections.category-section') --}}
+
+<!--=== ABOUT SECTION ===-->
+{{-- @include('frontend.pages.home.sections.about-section') --}}
+
+<!--=== COURSES SECTION ===-->
+{{-- @include('frontend.pages.home.sections.courses-section') --}}
+
+<!--=== OFFER START ===-->
+{{-- @include('frontend.pages.home.sections.offer-section') --}}
+
+<!--=== BECOME INSTRUCTOR START ===-->
+{{-- @include('frontend.pages.home.sections.become-instructor-section') --}}
+
+<!--=== VIDEO START ===-->
+{{-- @include('frontend.pages.home.sections.video-section') --}}
+
+<!--=== BRAND START ===-->
+{{-- @include('frontend.pages.home.sections.brand-section') --}}
+
+<!--=== QUALITY COURSES START ===-->
+{{-- @include('frontend.pages.home.sections.quality-course-section') --}}
+
+<!--=== TESTIMONIAL START ===-->
+{{-- @include('frontend.pages.home.sections.testimonial-section') --}}
+
+<!--=== BLOG 4 START ===-->
+{{-- @include('frontend.pages.home.sections.blog-section') --}}
+
+
+@endsection
